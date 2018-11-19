6aba8f6cdf638796f2b995480fdf173c329d091a
root <root@104906833.lasr.cs.ucla.edu>
2018-11-19 05:48:56 +0000

    Added group descriptor code

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-#NAME: Clayton Chu
+#NAME: Clayton Chu and George Zhang
 #ID: 104906833
 #EMAIL: claytonchu99@gmail.com
 
 CC=gcc
-FLAGS=-g -Wall -Wextra
+FLAGS=-g -Wall -Wextra -lm
 
 default:
 	$(CC) lab3a.c $(FLAGS) -o lab3a
--- lab3a.c
+++ lab3a.c
@@ -5,11 +5,12 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include "ext2_fs.h"
+#include <math.h>
 
 static int fd = 0;
 
 void analyze_superblock(struct ext2_super_block*);
-void analyze_groupdesc(struct ext2_group_desc*);
+void analyze_groupdesc(struct ext2_super_block*, struct ext2_group_desc*);
 void print_err(char* err){
   fprintf(stderr, "%s\n", err);
   exit(2);
@@ -33,13 +34,13 @@ int main(int argc, char** argv){
     print_err(strerror(errno));
 
   analyze_superblock(sb);
-  analyze_groupdesc(gd);
+  analyze_groupdesc(sb, gd);
   
   return 0;
 }
 
 void analyze_superblock(struct ext2_super_block* sb){
-  fprintf(stdout, "SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n",
+  printf("SUPERBLOCK,%d,%d,%d,%d,%d,%d,%d\n",
 	  sb->s_blocks_count,
 	  sb->s_inodes_count,
 	  EXT2_MIN_BLOCK_SIZE << sb->s_log_block_size,
@@ -49,7 +50,21 @@ void analyze_superblock(struct ext2_super_block* sb){
 	  sb->s_first_ino);
 }
 
-void analyze_groupdesc(struct ext2_group_desc* gd){
-  fprintf(stdout, "GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n",
-	  0, 0, 0, 0, 0, 0, 0, 0);
+void analyze_groupdesc(struct ext2_super_block* sb, struct ext2_group_desc* gd){
+  __u32 nGroups = ceil(((double)sb->s_blocks_count)/sb->s_blocks_per_group);
+  __u32 i;
+  for(i = 0; i < nGroups; i++){
+    __u32 totalBlocks = (sb->s_blocks_count % sb->s_blocks_per_group) ? sb->s_blocks_count % sb->s_blocks_per_group : sb->s_blocks_per_group; // if expression > 0, then it is the last block (remainder)
+    __u32 totalInodes = (sb->s_inodes_count % sb->s_inodes_per_group) ? sb->s_inodes_count % sb->s_inodes_per_group : sb->s_inodes_per_group; // if expression > 0, then it is the last block (remainder)
+    printf("GROUP,%d,%d,%d,%d,%d,%d,%d,%d\n",
+	   i,
+	   totalBlocks,
+	   totalInodes,
+	   gd->bg_free_blocks_count,
+	   gd->bg_free_inodes_count,
+	   gd->bg_block_bitmap,
+	   gd->bg_inode_bitmap,
+	   gd->bg_inode_table);
+  }
+	
 }
